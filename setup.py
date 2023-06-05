8996de54dc0e8f7b71588c58f957a23cebe71328
rotemzaif <rotem8zaif@gmail.com>
2023-06-05 13:55:21 +0300

    fixed the line with allure-pytest; '=' was missing

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='woocommercetest',
           "requests==2.28.2",
           "requests-oauthlib==1.3.1",
           "PyMySQL==1.0.2",
-          "allure-pytest=2.13.2",
+          "allure-pytest==2.13.2",
           "WooCommerce==3.0.0"
       ]
       )
